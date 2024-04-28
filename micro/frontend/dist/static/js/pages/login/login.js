957345383af7337a0b63a5e044325246d31c09d6
lujiangz <31911561+lujiangz@users.noreply.github.com>
2024-04-28 06:32:22 +0300

    text dosyasındaki hata çözüldü ama websocketlerinde hata var

--- micro/frontend/dist/static/js/pages/login/login.js
+++ micro/frontend/dist/static/js/pages/login/login.js
@@ -1,65 +1,127 @@
+import { setCookie } from '../../cookies/cookies.js';
+import changeContent from '../../uimodule/changeContent.js';
+import openSocket from '../../sockets/globalSocket.js';
+import openSocketPrivate from '../../sockets/privateSocket.js';
+import { selectTab } from '../../uimodule/chatBox.js';
+
+
+
+
 export default function login() {
 
-  document.getElementById('42intra').addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log('42 login');
-    window.location.href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-16b6c7462b738938b4c6b763f4d804a957769bb2e68fc5e727f86a1e219347e5&redirect_uri=https%3A%2F%2Flocalhost&response_type=code";
-  });
+ // login.js dosyası
 
-  document.getElementById('login-form').addEventListener('submit', async function(e) {
-    e.preventDefault();
-    const username_or_email = document.getElementById('InputUserOrEmail').value;
-    const password = document.getElementById('InputPassword').value;
-    try {
-      const response = await fetch('api/auth/login/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username_or_email, password })
-      });
-      const data = await response.json();
-      if (data.two_factor_required) {
-        const twoFactorCode = prompt('Please write 2FA code:');
-        if (twoFactorCode) {
-          const verifyResponse = await fetch('api/auth/verify-2fa/', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ user_username: data.user_username, token: twoFactorCode })
-          });
-          const verifyData = await verifyResponse.json();
-          if (verifyData.access){
-            setCookies(verifyData);
+document.getElementById('login-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const username_or_email = escapeHtml(document.getElementById('InputUserOrEmail').value);
+  const password = document.getElementById('InputPassword').value;
+
+  fetch('api/auth/login/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ username_or_email, password })
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.two_factor_required) {
+      // 2FA kodunu girmek için pop-up pencere aç
+      const twoFactorCode = prompt('Please write 2FA code:');
+      if (twoFactorCode) {
+        // 2FA kodunu doğrulamak için başka bir API isteği gönder
+        fetch('api/auth/verify-2fa/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ user_username: data.user_username, token: twoFactorCode })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.access){
+            setCookie('accessToken', data.access, {secure: true});
+            setCookie('refreshToken', data.refresh, {secure: true});
+            setCookie('username', escapeHtml(data.username), {secure: true}); // Escape username before setting cookie
             selectTab('tab1');
-            openSockets();
+            openSocket();
+            openSocketPrivate();
+            // setInterval(refreshAccessToken, 4 * 60 * 1000); 
+            // Login başarılı, ana sayfaya yönlendir
+            // window.location.href = '/user_profile'; // Örnek bir yönlendirme
             changeContent('home');
           }
-        }
-      } else if (data.access) {
-        setCookies(data);
-        selectTab('tab1');
-        openSockets();
-        changeContent('home');
-      } else {
-        alert('Login failed: ' + data.error);
+        })
+        .catch(error => {
+          console.error('2FA authentication error:', error);
+        });
       }
-    } catch (error) {
-      console.error('An error occurred during the login process:', error);
+    } else if (data.access) {
+      setCookie('accessToken', data.access, {secure: true});
+      setCookie('refreshToken', data.refresh, {secure: true});
+      setCookie('username', escapeHtml(data.username), {secure: true}); // Escape username before setting cookie
+      selectTab('tab1');
+      openSocket();
+      openSocketPrivate();
+      // setInterval(refreshAccessToken, 4 * 60 * 1000); 
+      // Login başarılı, ana sayfaya yönlendir
+      // window.location.href = '/user_profile'; // Örnek bir yönlendirme
+      changeContent('home');
+    } else {
+      // Hata mesajını göster
+      alert('Login failed: ' + data.error);
     }
+  })
+  .catch(error => {
+    console.error('An error occurred during the login process:', error);
   });
+});
 
-  function setCookies(data) {
-    setCookie('accessToken', data.access, { secure: true });
-    setCookie('refreshToken', data.refresh, { secure: true });
-    setCookie('username', data.username, { secure: true });
-  }
 
-  function openSockets() {
-    openSocket();
-    openSocketPrivate();
-  }
 }
 
-// xss'e parmak attım aynı mustiye attığım gibi
+ 
+
+// export default function login() {
+//     // Giriş formu işlevselliğini burada etkinleştir
+//     document.getElementById('login-form').addEventListener('submit', function(e) {
+//       e.preventDefault();
+//       const username_or_email = document.getElementById('InputUserOrEmail').value;
+//       const password = document.getElementById('InputPassword').value;
+//       fetch('api/auth/login/', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ username_or_email, password })
+//       })
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         if (data.access) {
+//           setCookie('accessToken', data.access, {secure: true});
+//           setCookie('refreshToken', data.refresh, {secure: true});
+//           setCookie('username', data.username, {secure: true});
+//           selectTab('tab1');
+//           openSocket();
+//           openSocketPrivate();
+//           // setInterval(refreshAccessToken, 4 * 60 * 1000); 
+//           // Login başarılı, ana sayfaya yönlendir
+//           // window.location.href = '/user_profile'; // Örnek bir yönlendirme
+//           changeContent('home');
+
+//         } else {
+//           // Hata mesajını göster
+//           alert('Giriş başarısız: ' + data.error);
+//         }
+//       })
+//       .catch(error => {
+//         console.error('There has been a problem with your fetch operation:', error);
+//       });
+//     });
+//   }
