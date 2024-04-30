cc21dee163a80285eae4f7767cf83e4eafbc94f0
lujiangz <31911561+lujiangz@users.noreply.github.com>
2024-04-30 15:53:06 +0300

    django-secret-key-env

--- micro/backend/teletubbies/testlol/settings.py
+++ micro/backend/teletubbies/testlol/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t7jh1*8j5fuof+p8yfbqjnpfjohw5cuq1=+93e$3th%4)6@($l'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
